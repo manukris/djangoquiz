887d659a8f9a4d2c6c60ad47823d9a058efa9aae
manukris <manukrishnatesro@gmail.com>
2019-03-09 14:50:43 +0530

    django quiz Questions added

--- onlinequiz/views.py
+++ onlinequiz/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import CreateView,ListView,UpdateView
+from django.contrib.auth.models import
+from django.views.generic import CreateView,ListView,UpdateView,FormView
 from .models import Questions
+from .form import LoginForm
 
 # Create your views here.
+
+
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'auth/login.html'
+
+    # def form_valid(self, form):
+    #     auth_login(self.request, form.get_user())
+
 
 class AddQuestion(CreateView):
     model = Questions
