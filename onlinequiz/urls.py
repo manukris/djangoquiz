91242a940e187ed38ea8134fa1931ab8430aafe7
manukris <manukrishnatesro@gmail.com>
2019-03-18 18:06:23 +0530

    django quiz Questions added

--- onlinequiz/urls.py
+++ onlinequiz/urls.py
@@ -3,7 +3,7 @@ from .views import AddQuestion
 
 urlpatterns = [
 
-    path('add/',AddQuestion.as_view(),name = "addquestion"),
+    path('',AddQuestion.as_view(),name = "addquestion"),
 
    # path('login/',)
 ]
